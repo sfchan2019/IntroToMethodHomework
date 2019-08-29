0b58484e2c6bf4f8aa5df4aad968f321684c1a49
Seng Fai Chan <SChan@spartaglobal.com>
2019-08-29 16:12:22 +0100

    out parameter, default parament

--- IntroductionToMethod/Program.cs
+++ IntroductionToMethod/Program.cs
@@ -10,6 +10,27 @@ namespace IntroductionToMethod
     {
         static void Main(string[] args)
         {
+            IntroToMethods _ITM = new IntroToMethods();
+            string reverseName;
+            string fullName;
+            fullName = _ITM.ConcatFNameAndLName("First Name", "Last Name", out reverseName);
+            Console.WriteLine(reverseName);
+
+            Console.WriteLine(_ITM.ConcatFNameAndLNameWithDefault("asd"));
+        }
+    }
+
+    class IntroToMethods
+    {
+        public string ConcatFNameAndLName(string fname, string lname, out string reverseName)
+        {
+            reverseName = lname + " " + fname;
+            return fname + " " + lname;
+        }
+
+        public string ConcatFNameAndLNameWithDefault(string fname = "Legally" , string lname = "Blonde")
+        {
+            return fname + " " + lname;
         }
     }
 }
